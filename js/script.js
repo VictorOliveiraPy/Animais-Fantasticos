af41ba665e16761f981afece3ebc71634292372f
Victor Hugo <victorblog410@gmail.com>
2020-01-07 19:32:52 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line import/extensions
+import initScrollSuave from './modules/scroll-suave.js';
+// eslint-disable-next-line import/extensions
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+// eslint-disable-next-line import/extensions
+import initAccordion from './modules/accordion.js';
+// eslint-disable-next-line import/extensions
+import initTabNav from './modules/tabnav.js';
+// eslint-disable-next-line import/extensions
+import initModal from './modules/modal.js';
+// eslint-disable-next-line import/extensions
+import initTooltip from './modules/tooltip.js';
+// eslint-disable-next-line import/extensions
+import initDropdownMenu from './modules/dropdown-menu.js';
+// eslint-disable-next-line import/extensions
+import initMenuMobile from './modules/menu-mobile.js';
+// eslint-disable-next-line import/extensions
+import initFuncionamento from './modules/funcionamento.js';
+// eslint-disable-next-line import/extensions
+import initFetchAnimais from './modules/fetch-animais.js';
+// eslint-disable-next-line import/extensions
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
